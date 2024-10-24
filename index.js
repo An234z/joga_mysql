2eaaabd027bc4448c6b310c6bceabf10fcb0fcb4
Anni Voore <anni.voore@voco.ee>
2024-10-24 09:04:53 +0300

    Displayed author article list

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const hbs = require('express-handlebars');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
-
-
-
 const app = express();
-
-
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -20,12 +14,8 @@ app.engine('hbs', hbs.engine({
    layoutsDir: path.join(__dirname, 'views', 'layouts'),
 }));
 
-
 app.use(express.static('public'));
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const con = mysql.createConnection({
    host: "localhost",
@@ -34,11 +24,11 @@ const con = mysql.createConnection({
    database: 'joga_mysql',
 });
 
-
 con.connect(function(err) {
    if (err) throw err;
    console.log('Connected to joga_mysql db');
 });
+
 app.get('/', (req, res) => {
     let query = `
         SELECT article.*, author.name AS author_name 
@@ -55,52 +45,44 @@ app.get('/', (req, res) => {
     });
 });
 
-
-/*app.get('/', (req, res) => {
-   let query = "SELECT * FROM article";
-   let articles = []
-   con.query(query, (err, result) => {
-       if (err) throw err;
-       articles = result
-      
-
-
-   res.render('index', { 
-       title: 'Homepage',
-       subtitle: 'Yoga Blog',
-       articles: articles
-   });
- });
-});
 app.get('/article/:slug', (req, res) => {
-   let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
-   let article
-   console.log(query)
-   con.query(query, (err, result) => {
-       if (err) throw err;
-       article = result
-       console.log(article)
-       res.render('article', {
-           article: article
-       })
-   });
-}); */
-app.get('/article/:slug', (req, res) => {
-    let query = `SELECT article.*, author.name AS author_name FROM article 
-                 LEFT JOIN author ON article.author_id = author.id 
-                 WHERE article.slug="${req.params.slug}"`;
+    let query = `
+        SELECT article.*, author.name AS author_name, author.id AS author_id 
+        FROM article 
+        LEFT JOIN author ON article.author_id = author.id 
+        WHERE article.slug = "${req.params.slug}"
+    `;
     
     con.query(query, (err, result) => {
         if (err) throw err;
-        const article = result[0]; // eeldame, et saame ainult ühe artikli
-        console.log(article); // see peaks näitama artikli andmeid koos autori nimega
+        const article = result[0];
         res.render('article', {
             article: article
         });
     });
 });
 
+app.get('/author/:author_id', (req, res) => {
+    const authorId = req.params.author_id;
+
+    // Esimene päring autori nime saamiseks
+    let authorQuery = `SELECT * FROM author WHERE id = ${authorId}`;
+    con.query(authorQuery, (err, authorResult) => {
+        if (err) throw err;
+
+        // Teine päring autori artiklite saamiseks
+        let articlesQuery = `SELECT * FROM article WHERE author_id = ${authorId}`;
+        con.query(articlesQuery, (err, articlesResult) => {
+            if (err) throw err;
+            res.render('author', {
+                author: authorResult[0], // Autor on objekti esimene element
+                articles: articlesResult
+            });
+        });
+    });
+});
+
+
 app.listen(3004, () => {
    console.log('App is started at http://localhost:3004');
 });
-
