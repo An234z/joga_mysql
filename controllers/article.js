5b7d6e211b1f483a7b77eb9085a80cee20d36769
Anni Voore <anni.voore@voco.ee>
2024-10-25 10:00:22 +0300

    Created article controller and route, refactored application code

--- controllers/article.js
+++ controllers/article.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const path = require('path');
-const hbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+
 const mysql = require('mysql');
-
-const app = express();
-
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -14,26 +8,10 @@ const con = mysql.createConnection({
     database: 'joga_mysql',
 });
 
-
 con.connect(err => {
     if (err) throw err;
     console.log('Connected to joga_mysql db');
 });
-
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.engine('hbs', hbs.engine({
-    extname: 'hbs',
-    defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'views', 'layouts'),
-}));
-
-app.use(express.static('public'));
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const getAllArticles = (req, res) => {
     const query = `
@@ -51,10 +29,9 @@ const getAllArticles = (req, res) => {
     });
 };
 
-
 const getArticleBySlug = (req, res) => {
     const query = `
-        SELECT article.*, author.name AS author_name, author.id AS author_id
+        SELECT article.*, author.name AS author_name
         FROM article
         LEFT JOIN author ON article.author_id = author.id
         WHERE article.slug = "${req.params.slug}"
@@ -68,7 +45,6 @@ const getArticleBySlug = (req, res) => {
     });
 };
 
-
 const getAuthorArticles = (req, res) => {
     const authorId = req.params.author_id;
 
@@ -77,9 +53,6 @@ const getAuthorArticles = (req, res) => {
 
     con.query(authorQuery, (err, authorResult) => {
         if (err) throw err;
-        if (authorResult.length === 0) {
-            return res.status(404).send('Author not found');
-        }
         con.query(articlesQuery, (err, articlesResult) => {
             if (err) throw err;
             res.render('author', {
@@ -91,14 +64,8 @@ const getAuthorArticles = (req, res) => {
     });
 };
 
-
-app.get('/', getAllArticles);
-app.get('/article/:slug', getArticleBySlug);
-app.get('/author/:author_id', getAuthorArticles);
-
-
-const PORT = 3004; 
-app.listen(PORT, () => {
-    console.log(`App is started at http://localhost:${PORT}`);
-});
-
+module.exports = {
+    getAllArticles,
+    getArticleBySlug,
+    getAuthorArticles
+};
