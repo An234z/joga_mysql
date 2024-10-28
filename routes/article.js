8d056e9760428e56837c1e16a20b012202d272c4
An234z <153507899+An234z@users.noreply.github.com>
2024-10-28 19:33:12 +0200

    Created author controller and route, refactored application code

--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,7 @@
-// routes/article.js
 const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/articleController');
 
-// Define routes related to articles
 router.get('/', articleController.getAllArticles);
 router.get('/article/:slug', articleController.getArticleBySlug);
 
