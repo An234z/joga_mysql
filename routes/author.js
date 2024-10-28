86043c869c49141e3e8446f3326810243945773f
An234z <153507899+An234z@users.noreply.github.com>
2024-10-28 19:33:43 +0200

    Created author controller and route, refactored application code

--- routes/author.js
+++ routes/author.js
@@ -1,9 +1,7 @@
-// routes/author.js
 const express = require('express');
 const router = express.Router();
 const authorController = require('../controllers/authorController');
 
-// Define route for author by ID
 router.get('/:author_id', authorController.getAuthorArticles);
 
 module.exports = router;
